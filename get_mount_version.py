fb7b7cb3acb91747728924ca68c0c16fe11f6f9e
James Malone <jamalone@gmail.com>
2021-12-26 13:00:04 -0800

    Update get_mount_version.py

--- get_mount_version.py
+++ get_mount_version.py
@@ -2,4 +2,8 @@ import ioptron as iom
 import ioptron.ioptron as iopt 
 
 scope = iopt.ioptron('COM5')
-print(scope.mount_version)
+print("Version #:  {}".format(scope.mount_version))
+print("FW_MAIN:  {}".format(scope.firmware.mainboard))
+print("FW_HC:  {}".format(scope.firmware.hand_controller))
+print("FW_RA:  {}".format(scope.firmware.ra))
+print("FW_DEC:  {}".format(scope.firmware.dec))
